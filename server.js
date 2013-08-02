25ff3fa2775b3ff86c895c3091fc72cf45235312
Maria Gomez <mgomez@thoughtworks.com>
2013-08-02 16:41:43 -0400

    Maria: changed to use express

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-var http = require('http');
+var express = require("express");
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('alert("Hello World!");');
-}).listen(3000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:3000/');
+var app = express();
+app.use(express.logger());
+
+app.get('/js', function(request, response) {
+  response.setHeader('Content-Type', 'text/plain');
+  response.send('alert("Hello World!");');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
