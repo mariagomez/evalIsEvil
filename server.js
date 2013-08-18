3cc723ad10c27ba1b2df25d51ae19e0d13d65e36
Maria Gomez <mgomez@thoughtworks.com>
2013-08-18 11:17:55 -0400

    Maria: adding extra code in the js response. It returns a div to keep and some javascript code to copy into the HEAD. Also added two different ways of manipulating the DOM as I think querySelector does not work on IE7

--- server.js
+++ server.js
@@ -12,11 +12,19 @@ app.get('/', function(request, response) {
     });
 });
 
+app.get('/jquery.js', function(request, response) {
+  fs.readFile('jquery.js',function (err, data){
+        response.writeHead(200, {'Content-Type': 'text/plain','Content-Length':data.length});
+        response.write(data);
+        response.end();
+    });
+});
+
 
 app.get('/js', function(request, response) {
   response.setHeader('Content-Type', 'text/plain');
   response.setHeader('Access-Control-Allow-Origin', '*');
-  response.send('alert("Hello World!"); document.getElementById("result").innerHTML="RESULT!";');
+  response.send('<div id="toKeep"><p>Hello</p></div><script type="text/javascript">function foo() {alert("bar");}</script>');
 });
 
 var port = process.env.PORT || 5000;
